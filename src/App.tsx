9701e4742ae3bc487e6f61f6a1ead1ab40dd0f84
unknown <felipejmarsh@gmail.com>
2026-09-22 22:23:34 -0300

    fix token in request body

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
       {currentView === "admin" && currentUser.role === "admin" ? (
         <AdminPage onBackToUserView={() => setCurrentView("user-addresses")} />
       ) : (
-        <AddressesPage />
+        <AddressesPage userId={currentUser.id} />
       )}
     </AppLayout>
   );
